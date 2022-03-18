f9ef2737bac4d77631ebfe0ba125fb204ccdb531
Atishey jain <atisheyjain1232@gmail.com>
2022-03-18 15:11:23 +0530

    socket fixes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 // routes
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Router from './routes';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Router from "./routes";
 import { io } from "socket.io-client";
-import ThemeConfig from './theme';
-import { useSelector, useDispatch } from 'react-redux';
-import GlobalStyles from './theme/globalStyles';
-import ScrollToTop from './components/ScrollToTop';
-import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
-import { loggedInUser } from './redux/actions/auth.action';
+import ThemeConfig from "./theme";
+import { useSelector, useDispatch } from "react-redux";
+import GlobalStyles from "./theme/globalStyles";
+import ScrollToTop from "./components/ScrollToTop";
+import { BaseOptionChartStyle } from "./components/charts/BaseOptionChart";
+import { loggedInUser } from "./redux/actions/auth.action";
 // ----------------------------------------------------------------------
 
-const SERVER_URL = process.env.REACT_APP_API_URL
+const SERVER_URL = process.env.REACT_APP_API_URL;
 
 export default function App() {
   const navigate = useNavigate();
-  const { isLoggedIn } = useSelector(state => state.auth);
+  const { isLoggedIn } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
+    var socket = io(SERVER_URL);
     if (!isLoggedIn && !token) {
-      navigate('/', { replace: true })
-    } else {
-      // const userData = JwtDecode(token);
-      // dispatch(loggedInUser(userData));
+      navigate("/", { replace: true });
+    } else if (!isLoggedIn && token) {
+      console.log("🚀 ~ file: App.js ~ line 19 ~ App ~ isLoggedIn", isLoggedIn);
       dispatch(loggedInUser(JSON.parse(token)));
-      navigate('/dashboard/app', { replace: true })
+      navigate("/dashboard/app", { replace: true });
+      socket.emit("connected", {
+        user: JSON.parse(token),
+      });
+      socket.on("disconnect", () => {
+        socket.emit("disconnected", {
+          user: JSON.parse(token),
+        });
+      });
     }
-    const socket = io(SERVER_URL);
-    socket.emit('connected')
-  }, [isLoggedIn])
+  }, [isLoggedIn]);
 
   return (
     <ThemeConfig>
